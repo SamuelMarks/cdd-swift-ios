2cc69e86a8584246b1db2054408d489f2ce56cce
Alexei Rudakov <rudakov3000@mail.ru>
2019-07-12 18:39:39 +0800

    temp Generating requests

--- Sources/cdd-swift/Cli/main.swift
+++ Sources/cdd-swift/Cli/main.swift
@@ -2,15 +2,18 @@ import Foundation
 import SwiftSyntax
 import SwiftCLI
 
-let cli = CLI(
-    name: "cdd-swift",
-    version: "0.1.0",
-    description: "Compiler Driven Development: Swift Adaptor",
-    commands: [
-        GenerateCommand(),
-        SyncCommand(),
-        TestCommand()
-    ]
-)
 
-cli.goAndExit()
+try? SyncCommand().execute()
+
+//let cli = CLI(
+//    name: "cdd-swift",
+//    version: "0.1.0",
+//    description: "Compiler Driven Development: Swift Adaptor",
+//    commands: [
+//        GenerateCommand(),
+//        SyncCommand(),
+//        TestCommand()
+//    ]
+//)
+//
+//cli.goAndExit()
