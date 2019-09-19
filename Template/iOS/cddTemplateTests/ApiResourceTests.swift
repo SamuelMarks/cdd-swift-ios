32c8f63262dcac0dd1d9e93d85f305d618f221fd
Rob Saunders <rob@wearebrandnew.com>
2019-09-19 17:31:59 +0800

    fixed tests incorrectly named in template

--- Template/iOS/cddTemplateTests/ApiResourceTests.swift
+++ Template/iOS/cddTemplateTests/ApiResourceTests.swift
@@ -2,26 +2,4 @@ import XCTest
 @testable import codetester
 
 class codetesterTests: XCTestCase {
-
-	// GET /pets/{id}
-	func testGetPet() {
-		let request = PetsPetIdGetRequest()
-
-		request.send(
-			client: MockClient(json: #"{"id": 1, "name": 1}"#, statusCode: 200),
-			onResult: { pet in /*ok*/ },
-			onError: { error in XCTFail("onError: \(error)") },
-			onOtherError: { error in XCTFail("onOtherError: \(error)") })
-	}
-
-	// GET /pets/
-	func testGetPets() {
-		let request = PetsGetRequest(limit: 1)
-
-		request.send(
-			client: MockClient(json: #"[{"id": 1}]"#, statusCode: 200),
-			onResult: { pet in /*ok*/ },
-			onError: { error in XCTFail("onError: \(error)") },
-			onOtherError: { error in XCTFail("onOtherError: \(error)") })
-	}
 }
